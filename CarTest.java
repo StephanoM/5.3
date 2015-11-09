f6d3a4b06ab5f93d4b416c99d9110f489f620b97
StephanoM <murostephano@gmail.com>
2015-11-09 17:37:32 -0500

    Update and rename Person.java to CarTest.java

---
+++ CarTest.java
@@ -0,0 +1,43 @@
+
+public class CarTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car person1 = new Car("VW",10,10, "Green");
+		person1.setmake("VW");
+		person1.getmake();
+		person1.setengine(10);
+		person1.getengine();
+		person1.sethpw(10);
+		person1.gethpw();
+		person1.setColor("Green");
+		person1.getColor();
+		System.out.println(person1);
+		
+		Car person2 = new Car("BMW",102,102, "Blue");
+		person2.setmake("BMW");
+		person2.getmake();
+		person2.setengine(10);
+		person2.getengine();
+		person2.sethpw(10);
+		person2.gethpw();
+		person2.setColor("Blue");
+		person2.getColor();
+		System.out.println(person2);
+		
+		Car person3 = new Car("VW",100,100, "Yellow");
+		person3.setmake("VW");
+		person3.getmake();
+		person3.setengine(10);
+		person3.getengine();
+		person3.sethpw(10);
+		person3.gethpw();
+		person3.setColor("Yellow");
+		person3.getColor();
+		System.out.println(person3);
+		
+		
+
+	}
+
+}
